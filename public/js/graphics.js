db40c085ba24d148f7a39475280c18c7d94459b5
Rahul <rdk750@gmail.com>
2018-03-02 16:11:33 -0500

    created test cube

--- public/js/graphics.js
+++ public/js/graphics.js
@@ -3,9 +3,20 @@
   * @author Rahul Kiefer
   */
 
+
+//created scene and camera
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+// created renderer
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+//creating cube
+var geometry = new THREE.BoxGeometry(5,5,5);
+var material = new THREE.MeshBasicMaterial( {color: 0x00ff00 } );
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
