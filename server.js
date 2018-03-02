9b423fbbf248970fe277b037965fc105462186b2
Jonathan Lam <jlam55555@gmail.com>
2018-03-02 18:44:33 -0500

    added basic rooms

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*
+/**
   * Basic app routing using express
   * @author Jonathan Lam
   */
@@ -8,14 +8,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-// statically serve files in the public directory as root
-app.use(express.static('public'));
-
 // set port to environment-defined port or 5000 (default)
 http.listen(
   process.env.PORT || 5000,
   () => console.log(`Listening on port ${process.env.PORT || 5000}.`));
-
 
 /**
   * Get socket.io dependency
@@ -24,12 +20,24 @@ http.listen(
 
 // socket.io for real-time WebSocket communication
 var io = require('socket.io')(http);
+var session = require('express-session')({
+    secret: 'test-secret',
+    resave: true,
+    saveUninitialized: true
+});
+app.use(session);
+var sharedsession = require('express-socket.io-session');
+io.use(sharedsession(session, { autoSave: true }));
 
 // callback to listen for io events
 io.on('connection', socket => {
-  
+
   // handle when a person connects
   console.log(`A user with socket id ${socket.id} has connected.`);
+
+  // sync up to express
+  socket.handshake.session.socketId = socket.id;
+  socket.handshake.session.save();
 
   // handle when a person disconnects
   socket.on('disconnect', () => {
@@ -37,3 +45,28 @@ io.on('connection', socket => {
   });
 
 });
+
+/**
+  * Rooms to allow people
+  * @author Jonathan Lam
+  */
+app.get('/game/:gameid', (req, res, next) => {
+  // get gameid parameter
+  var gameid = req.params.gameid;
+  var socket;
+
+  // sync up to socket to join room (keep refreshing until socketId is updated)
+  var syncInterval = setInterval(() => req.session.reload(() => {
+    if(req.session.socketId !== undefined && (socket = io.sockets.sockets[req.session.socketId]) !== undefined) {
+      clearInterval(syncInterval);
+
+      // this code will run when corresponding socket is found
+      socket.join(gameid);
+      io.to(gameid).emit('message', `testing: you are in room ${gameid}`);
+    }
+  }), 50);
+
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.use(express.static('public'));
