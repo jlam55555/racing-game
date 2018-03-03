8cb0446bd4a8c3bde89aa37528cacc0ff394ed49
Jonathan Lam <jlam55555@gmail.com>
2018-03-02 19:49:06 -0500

    added basic join game functionality from homepage

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,16 @@
   * @author Jonathan Lam
   */
 var socket = io();
+
+/**
+  * Join a room when button is clicked
+  * @todo   Add server-side verification before submitting
+  * @author Jonathan Lam
+  */
+var joinGameId = document.querySelector('#joinGameId');
+var joinGameButton = document.querySelector('#joinGame');
+joinGameButton.addEventListener('click', () => {
+
+  window.location.href = `${window.location.href}game/${joinGameId.value}`;
+
+})
