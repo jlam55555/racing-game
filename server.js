136a0c29768cf75dfc70e23679c7ec41dda6fedb
Jonathan Lam <jlam55555@gmail.com>
2018-03-19 08:14:35 -0400

    fixed syntax error

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ setInterval(() => {
     for(var client of rooms[room].clients) {
 
       // add simulated friction
-      acceleration -= speed * frictionMultiplier;
+      acceleration -= client.speed * frictionMultiplier;
 
       // update player speed
       client.speed += client.acceleration * accelerationMultiplier;
